295453f861a7f8002e805d4a94a43c2f9f7d48e4
Dale Jung <dale@dalejung.com>
2014-04-03 01:16:39 -0400

    skeleton

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+DISTNAME='edamame'
+FULLVERSION='0.1'
+
+setup(
+    name=DISTNAME,
+    version=FULLVERSION,
+    packages=['edamame'],
+    entry_points={
+    }
+      )
